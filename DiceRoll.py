b761c6ca87171e02f0b52f19f5cf93e0194e2c55
jonas <jonas.boucher@lakeregionschools.org>
2025-11-06 09:40:18 -0500

    Initial commit

---
+++ DiceRoll.py
@@ -0,0 +1,20 @@
+import random
+
+def diceRoll():
+    diceValue = random.randint(1,6)
+    return diceValue
+
+def startGame():
+    print("Welcome to Dice game! Its its your roll vs. theirs")
+    ourRoll = diceRoll()
+    theirRoll = diceRoll()
+    print("you rolled a " + str(ourRoll))
+    print("you opponent rolled a " + str(theirRoll))
+    if ourRoll == theirRoll:
+        print("tie")
+    elif ourRoll > theirRoll:
+        print("you win")
+    else:
+        print("you lose")
+
+startGame()
